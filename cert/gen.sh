821c305258b7e21881e816ccdd3171afb90ebacb
Tech School <techschool.guru@gmail.com>
2020-04-24 11:14:14 +0200

    Fix cert generation script

--- cert/gen.sh
+++ cert/gen.sh
@@ -18,7 +18,7 @@ openssl x509 -in server-cert.pem -noout -text
 # 4. Generate client's private key and certificate signing request (CSR)
 openssl req -newkey rsa:4096 -nodes -keyout client-key.pem -out client-req.pem -subj "/C=FR/ST=Alsace/L=Strasbourg/O=PC Client/OU=Computer/CN=*.pcclient.com/emailAddress=pcclient@gmail.com"
 
-# 3. Use CA's private key to sign client's CSR and get back the signed certificate
+# 5. Use CA's private key to sign client's CSR and get back the signed certificate
 openssl x509 -req -in client-req.pem -days 60 -CA ca-cert.pem -CAkey ca-key.pem -CAcreateserial -out client-cert.pem -extfile client-ext.cnf
 
 echo "Client's signed certificate"
